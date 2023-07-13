516e09711811bbe65a294d0f4ca96c145a2028f5
hagarhasheshh <hagarhashesh@std.mans.edu.eg>
2023-07-13 21:18:46 +0300

    Chapter 4 code + quicksort algorithm

--- Chapter4.c
+++ Chapter4.c
@@ -3,15 +3,21 @@
 int sumOfArray(int arr[] , int size) ;
 int count (int arr[] , int size) ;
 int maxNumber(int arr[] , int size);
-
+void swap(int* a, int* b);
+int partition(int arr[], int low, int high);
+void quickSort(int arr[], int low, int high);
+void printArray(int arr[], int size);
 
 int main(void)
 {
-	int arr[] = {4 , 2 , 3 , 1 ,6};
+	int arr[] = {4 , 2 , 3 , 1 ,6, 5};
 	int size = sizeof(arr)/sizeof(arr[0]);
-	printf("The sum of elements is %d" , sumOfArray(arr , size));
-	printf("The number of elements is %d" , count (arr , size));
-	printf("Max : %d" , maxNumber(arr,size));
+	printf("The number of elements is %d\n" , count (arr , size));
+	printf("Max : %d\n" , maxNumber(arr,size));
+	quickSort(arr, 0, size - 1);
+    printf("Sorted array: ");
+    printArray(arr, size);
+	printf("The sum of elements is %d\n" , sumOfArray(arr , size));
 	return 0;
 }
 int sumOfArray(int arr[] , int size)
@@ -66,4 +72,39 @@ int maxNumber(int arr[] , int size)
     {
         return subMax ;
     }
+}
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot_index = partition(arr, low, high);
+
+        quickSort(arr, low, pivot_index - 1);
+        quickSort(arr, pivot_index + 1, high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
